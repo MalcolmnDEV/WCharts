f1e1dd04d652b1091dd795b6666547c66b755ab2
MalcolmnDEV <robertsmalcolmn@gmail.com>
2022-06-20 17:01:22 +0100

    Initial  commit

---
+++ Example/Pods/Target Support Files/Pods-WCharts_Tests/Pods-WCharts_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WCharts_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WCharts_Tests
+@end
---
+++ Example/Pods/Target Support Files/WCharts/WCharts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WCharts : NSObject
+@end
+@implementation PodsDummy_WCharts
+@end
